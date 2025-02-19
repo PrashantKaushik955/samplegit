322f4c6acc1a84a436433c4fca2d389217b04379
Prashant Kaushik <prashant.kaushik@techugo.com>
2025-02-19 15:46:39 +0530

    add feature1

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const app = express();
 
 app.use('/api/posts',postRoutes);
 
+feature1 
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
